c178385f70c4168210f1a42cfcba08a324de0bb5
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-05 10:39:19 -0500

    Add hide Menu function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+function hideMenu(item) {
+  item.addEventListener('click', () => {
+    const menuCanvas = document.querySelector('.interactive-menu');
+    menuCanvas.remove();
+  });
+}
+
 function displayMenu() {
   const menuIcon = document.querySelector('nav.menu-button');
   const menuCanvas = document.createElement('nav');
@@ -28,6 +35,9 @@ function displayMenu() {
   menuOption.textContent = 'Contact';
   menuOption.classList.add('menu-option');
   menuCanvas.appendChild(menuOption);
+
+  const menuItems = document.querySelectorAll('.interactive-menu > *');
+  menuItems.forEach(hideMenu);
 }
 
 const menuBtn = document.querySelector('.menu-anchor');
