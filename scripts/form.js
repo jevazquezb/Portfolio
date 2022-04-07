6c8fd2c6e867002477675fcd1c04d6d02ed3b3eb
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-07 11:31:36 -0500

    Fix validation styles

--- scripts/form.js
+++ scripts/form.js
@@ -1,21 +1,20 @@
-const form = document.querySelector('#form');
+const form = document.querySelector('#contact-form');
 const email = document.querySelector('#email');
 
 const errorMsgContainer = document.querySelector('.msg-cont');
 const errorMsg = document.querySelector('.error-msg');
 
-form.addEventListener('submit',validateEmail);
+form.addEventListener('submit', validateEmail);
 
-function validateEmail(event) {    
-    
-    if (email.value === email.value.toLowerCase()) {
-        errorMsgContainer.classList.add('hidden');
-        errorMsg.textContent = '';
-        email.classList.remove('highlight');
-    } else {
-        errorMsgContainer.classList.remove('hidden');
-        errorMsg.textContent = 'Email should be in lower case';
-        email.classList.add('highlight');
-        event.preventDefault();
-    }
+function validateEmail(event) {
+  if (email.value === email.value.toLowerCase()) {
+    errorMsgContainer.classList.add('hidden');
+    errorMsg.textContent = '';
+    email.classList.remove('highlight');
+  } else {
+    errorMsgContainer.classList.remove('hidden');
+    errorMsg.textContent = 'Email should be in lower case';
+    email.classList.add('highlight');
+    event.preventDefault();
+  }
 }
